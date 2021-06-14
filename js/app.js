a07b18660ff12583fc1c54311994ee4802e81dfe
Bezaleel Ashefor <knightbenax@gmail.com>
2021-06-14 17:51:03 +0100

    New Additions

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 
 $(document).ready(function() {
-  console.log("Hi there! Say hello on Twitter - @knightbenax")
+  console.log("Hi there! Say hello on Twitter or Origin - @knightbenax")
   var $nav = $(".menu");
   if ($nav.css("display") != 'none'){
       $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   }
+
+  doResize();
 });
 
 $(document).scroll(function () {
@@ -13,3 +15,10 @@ $(document).scroll(function () {
         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     }
 });
+
+function doResize(){
+    if ($(window).width() > 768){
+        const avatarWidth = $(".avatar").outerWidth(true);
+        $(".sectiontag").width(avatarWidth);
+    }
+}
