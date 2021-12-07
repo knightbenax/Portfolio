e1de8412b30f77b26dd3c9bbdf988d942f09caaa
Bezaleel Ashefor <knightbenax@gmail.com>
2021-12-08 00:18:27 +0100

    Rains

--- js/app.js
+++ js/app.js
@@ -7,6 +7,17 @@ $(document).ready(function() {
   }
 
   doResize();
+
+  var docWidth = document.documentElement.offsetWidth;
+
+[].forEach.call(
+  document.querySelectorAll('*'),
+  function(el) {
+    if (el.offsetWidth > docWidth) {
+      console.log(el);
+    }
+  }
+);
 });
 
 $(document).scroll(function () {
